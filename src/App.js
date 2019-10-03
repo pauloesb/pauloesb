b44741f7e26b24ae6c0eff60dbe0906b05c20e87
Paulo Borba <pauloesb@gmail.com>
2019-10-03 14:37:35 -0300

    Implementing Contact two way data binding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Sidebar from './components/UI/Sidebar';
 import Body from './components/containers/Body';
+import Footer from './components/UI/Footer';
 import './App.sass';
 
 function App() {
   return (
-    <div className="main-app columns">
-      <Sidebar />
-      <Body />
-    </div>
+    <>
+      <div className="main-app columns">
+        <Sidebar />
+        <Body />
+      </div>
+      <Footer />
+    </>
   );
 }
 
--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,67 +1,110 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Contact.module.css';
 import reveal from '../../HOC/reveal';
 
-const contact = () => {
+const Contact = () => {
+  const [form, setForm] = useState({
+    name: null,
+    email: null,
+    message: null,
+    sendingMessage: false
+  });
+
+  const handlerForm = event => {
+    const newForm = { ...form };
+    // eslint-disable-next-line default-case
+    switch (event.target.id) {
+      case 'name':
+        setForm({ ...newForm, name: event.target.value });
+        break;
+      case 'email':
+        setForm({ ...newForm, email: event.target.value });
+        break;
+      case 'message':
+        setForm({ ...newForm, message: event.target.value });
+        break;
+    }
+  };
+
+  const sendMail = event => {
+    event.preventDefault();
+    console.log(form);
+  };
+
   return (
     <div id="contact" className={`${classes.contactContainer} container`}>
       <h1 className="title">Contact</h1>
-      <form className={`${classes.contactForm} box column is-7`}>
-        <p className={`${classes.contactFormText} content`}>
-          Send me a message to my personal email{' '}
-          <a href="mailto:pauloesb@gmail.com">pauloesb@gmail.com</a> or send a
-          message through the contact form below, let&#39;s talk and work
-          together...
-        </p>
-        <div className={`${classes.sideBySide} container is-flex`}>
-          <div className="field column is-6">
-            <label htmlFor="name" className={`${classes.contactLabel} label`}>
-              Name
-            </label>
-            <div className="control">
-              <input
-                id="name"
-                className="input"
-                type="text"
-                placeholder="Name"
-              />
-            </div>
-          </div>
-          <div className="field column is-6">
-            <label htmlFor="email" className={`${classes.contactLabel} label`}>
-              E-mail
-            </label>
-            <div className="control">
-              <input
-                id="email"
-                className="input"
-                type="email"
-                placeholder="E-mail"
-              />
-            </div>
-          </div>
-        </div>
-        <div className={`${classes.sideBySide} container is-flex`}>
-          <div className="field column is-12">
-            <label className={`${classes.contactLabel} label`}>Message</label>
-            <textarea className="textarea" placeholder="Your message" />
-          </div>
-        </div>
-        <div
-          className={`${classes.sideBySide} ${classes.btnSubmit} container is-flex`}
+      <div className={classes.contactSubContainer}>
+        <form
+          onSubmit={sendMail}
+          className={`${classes.contactForm} box column is-10`}
         >
-          <div className="field">
-            <div className="control">
-              <button type="submit" className="button is-link">
-                Submit
-              </button>
+          <p className={`${classes.contactFormText} content`}>
+            Send me a message to my personal email{' '}
+            <a href="mailto:pauloesb@gmail.com">pauloesb@gmail.com</a> or send a
+            message through the contact form below, let&#39;s talk and work
+            together...
+          </p>
+          <div className={`${classes.sideBySide} container is-flex`}>
+            <div className="field column is-6">
+              <label htmlFor="name" className={`${classes.contactLabel} label`}>
+                Name
+              </label>
+              <div className="control">
+                <input
+                  id="name"
+                  className="input"
+                  type="text"
+                  placeholder="Name"
+                  onChange={handlerForm}
+                />
+              </div>
+            </div>
+            <div className="field column is-6">
+              <label
+                htmlFor="email"
+                className={`${classes.contactLabel} label`}
+              >
+                E-mail
+              </label>
+              <div className="control">
+                <input
+                  id="email"
+                  className="input"
+                  type="email"
+                  placeholder="E-mail"
+                  onChange={handlerForm}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+          <div className={`${classes.sideBySide} container is-flex`}>
+            <div className="field column is-12">
+              <label className={`${classes.contactLabel} label`}>Message</label>
+              <textarea
+                id="message"
+                className="textarea"
+                placeholder="Your message"
+                onChange={handlerForm}
+              />
+            </div>
+          </div>
+          <div
+            className={`${classes.sideBySide} ${classes.btnSubmit} container is-flex`}
+          >
+            <div className="field">
+              <div className="control">
+                <button type="submit" className="button is-link">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default reveal(contact);
+export default reveal(Contact);
